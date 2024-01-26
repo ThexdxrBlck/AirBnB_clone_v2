c549ff8fbd93267d6146de964ad9e563266e99d3
ThexdxrBlck <morobe76o@gmail.com>
2024-01-26 11:06:27 +0000

    new task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
+The application listens on 0.0.0.0, port 5000
 """
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
+@app.route('/', strict_slashes=False)
+def hello():
     """Returns given string"""
     return render_template("10-hbnb_filters.html")
 
